92edf362d94e97a0abed6bc716c01588770e2c08
Jossman <derek leon>
2019-12-29 04:56:39 +0700

    update commit

--- end.js
+++ end.js
@@ -10,21 +10,21 @@ const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
-  saveScoreBtn.disabled = !username.value;
+    saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = e => {
-  console.log("clicked the save button!");
-  e.preventDefault();
+    console.log("clicked the save button!");
+    e.preventDefault();
 
-  const score = {
-    score: Math.floor(Math.random() * 100),
-    name: username.value
-  };
-  highScores.push(score);
-  highScores.sort((a, b) => b.score - a.score);
-  highScores.splice(5);
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
 
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("quiz.html");
 };
